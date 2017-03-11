246cb8240d19a5fcfceee52cdbdc3e79448c5888
Jelle Smet <development@smetj.net>
2017-03-11 18:01:28 +0100

    bump version and remove version pin for requests

--- setup.py
+++ setup.py
@@ -27,11 +27,11 @@ from setuptools.command.test import test as TestCommand
 import sys
 
 PROJECT = 'wishbone_input_httpclient'
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 
 install_requires = [
     'wishbone>=2.1.1',
-    'requests==2.9.1'
+    'requests'
 ]
 
 try:
